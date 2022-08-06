be4e412d3583e9206c59d650ebabe2a753008d81
Ruben Martinez Jr <rubencodes@users.noreply.github.com>
2022-08-06 12:28:48 -0400

    Use `ref_name` instead of `ref`
    Update config.js
    
    Update vercel.js
    
    Update action.yml
    
    Update action.yml

--- dist/index.js
+++ dist/index.js
@@ -15340,6 +15340,7 @@ const setDynamicVars = () => {
 		context.PR_NUMBER = github.context.payload.number
 		context.ACTOR = github.context.payload.pull_request.user.login
 		context.REF = github.context.payload.pull_request.head.ref
+		context.REF_NAME = github.context.payload.pull_request.head.ref_name
 		context.SHA = github.context.payload.pull_request.head.sha
 		context.BRANCH = github.context.payload.pull_request.head.ref
 		context.IS_FORK = github.context.payload.pull_request.head.repo.full_name !== context.GITHUB_REPOSITORY
@@ -15365,6 +15366,7 @@ core.debug(
 )
 
 module.exports = context
+
 
 /***/ }),
 
@@ -15571,6 +15573,7 @@ const {
 	USER,
 	REPOSITORY,
 	REF,
+	REF_NAME,
 	TRIM_COMMIT_MESSAGE,
 	BUILD_ENV,
 	WORKING_DIRECTORY,
@@ -15606,7 +15609,7 @@ const init = () => {
 				`githubCommitMessage=${ TRIM_COMMIT_MESSAGE ? commit.commitMessage.split(/\r?\n/)[0] : commit.commitMessage }`,
 				`githubCommitOrg=${ USER }`,
 				`githubCommitRepo=${ REPOSITORY }`,
-				`githubCommitRef=${ REF }`,
+				`githubCommitRef=${ REF_NAME }`,
 				`githubCommitSha=${ SHA }`,
 				`githubOrg=${ USER }`,
 				`githubRepo=${ REPOSITORY }`,
@@ -15671,6 +15674,7 @@ const init = () => {
 module.exports = {
 	init
 }
+
 
 /***/ }),
 
